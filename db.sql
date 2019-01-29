1976e07ce87790f0bfad1e1f51e38b45d067b42d
alex <genby8@gmail.com>
2019-01-29 10:33:37 +0600

    БД запрос к комиту 02851367fedcb8f19ba27dd1cc7f80091d91ef22 (оптимизация баланса пользователя)

--- db.sql
+++ db.sql
@@ -2,7 +2,21 @@
 /*!40101 SET NAMES utf8 */;
 /*!50503 SET NAMES utf8mb4 */;
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE=''NO_AUTO_VALUE_ON_ZERO'' */;
+
+CREATE TABLE IF NOT EXISTS `account` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `uid` int(11) unsigned NOT NULL,
+  `balance` decimal(16,2) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `FK_account_users` (`uid`),
+  CONSTRAINT `FK_account_users` FOREIGN KEY (`uid`) REFERENCES `users` (`id`) ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+
+/*!40000 ALTER TABLE `account` DISABLE KEYS */;
+INSERT INTO `account` (`id`, `uid`, `balance`) VALUES
+	(1, 1, 972.00);
+/*!40000 ALTER TABLE `account` ENABLE KEYS */;
 
 CREATE TABLE IF NOT EXISTS `account_transaction` (
   `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
@@ -17,7 +31,7 @@ CREATE TABLE IF NOT EXISTS `account_transaction` (
 
 /*!40000 ALTER TABLE `account_transaction` DISABLE KEYS */;
 INSERT INTO `account_transaction` (`id`, `uid`, `amount`, `datetime`) VALUES
-	(1, 1, 972.00, '2019-01-28 11:17:29');
+	(1, 1, 972.00, ''2019-01-28 11:17:29'');
 /*!40000 ALTER TABLE `account_transaction` ENABLE KEYS */;
 
 CREATE TABLE IF NOT EXISTS `users` (
@@ -31,9 +45,9 @@ CREATE TABLE IF NOT EXISTS `users` (
 
 /*!40000 ALTER TABLE `users` DISABLE KEYS */;
 INSERT INTO `users` (`id`, `email`, `salt`, `password`) VALUES
-	(1, 'email@domain.local', 'salt', '83234657c5df8232839ac8c0572e158d');
+	(1, ''email@domain.local'', ''salt'', ''83234657c5df8232839ac8c0572e158d'');
 /*!40000 ALTER TABLE `users` ENABLE KEYS */;
 
-/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
+/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '''') */;
 /*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
